04c87b5068997238c86d5d35563f4d17a9349c01
Andras Nemes <nemes1988@icloud.com>
2018-06-27 14:12:57 +0200

    Well the book was obviously wrong.

---
+++ static/script.js
@@ -0,0 +1,15 @@
+myFunction = () => {
+  document.querySelector('#myDropdown').classList.toggle('show');
+};
+
+window.onclick = event => {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.querySelectorAll('.dropdown-content');
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
